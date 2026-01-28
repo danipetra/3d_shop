dff194bc889195f58266baa7625d539d7c63efde
Daniele Petracca <daniele.petracca.dp@gmail.com>
2026-01-29 00:22:20 +0100

    feat: implemented navbar and logo

---
+++ app/components/Logo.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface LogoProps{
+    size?:'sm' | 'lg';
+}
+
+const Logo = ({size='sm'}
+    :LogoProps) => {
+        const iconSize = size ===
+        "lg" ? 35 : 25;
+        return (
+          <div className="flex flex-row gap-2 items-center">
+            <Image src="/assets/logo.png" alt="logo" width={iconSize} height={iconSize}/>
+            <p className={`text-slate-400 ${size === 'sm' ? 'text-sm' : ""}`}>keyboard</p>
+          </div>
+        )
+}
+
+export default Logo
