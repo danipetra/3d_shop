3f0c920b489665c1389ac27661ba506f6038599c
Daniele Petracca <daniele.petracca.dp@gmail.com>
2026-01-29 21:01:15 +0100

    feat: added features cards section

--- app/components/sections/Features.tsx
+++ app/components/sections/Features.tsx
@@ -1,9 +1,46 @@
+"use client"
+
 import React from 'react'
 
-function Features() {
+import { FaGamepad, FaHandRock } from 'react-icons/fa';
+import { FaBolt, FaKeyboard } from 'react-icons/fa6';
+import FeatureCard from '../FeatureCard';
+
+const features = [
+  {
+    icon: FaKeyboard,
+    title: "Compact Design",
+    description: "Experience a sleek, compact design that frees up space for intense gaming sessions, allowing for more mouse movement and a cleaner setup."
+  },
+  {
+    icon: FaBolt,
+    title: "RGB Backlighting",
+    description: "Customize your keyboard with vibrant RGB backlighting, featuring dynamic lighting effects and millions of colors to match your gaming rig."
+  },
+  {
+    icon: FaHandRock,
+    title: "Mechanical Switches",
+    description: "Enjoy precise, tactile feedback with durable mechanical switches, ensuring fast response times and reliable performance during gameplay."
+  },
+  {
+    icon: FaGamepad,
+    title: "Gaming Mode",
+    description: "Activate gaming mode to disable the Windows key and avoid accidental interruptions, keeping you focused on the game."
+  }
+];
+
+const Features = () => {
   return (
-    <div>
-      Features
+    <div id='features' className='max-w-5xl mx-auto pt-8'>
+      <h2 className='text-2xl font-semibold pl-4 md:pl-16 pb-16'>
+        <span className='animate-pulse'>/</span>
+        features
+      </h2>
+      <div className='flex flex-wrap items-center justify-center gap-8 ,ax-w-4xl mx-auto'>
+        {features.map((features, index) => (
+          <FeatureCard key={index} index={index} icon={features.icon} title={features.title} description={features.description}/>
+        ))}
+      </div>
     </div>
   )
 }
