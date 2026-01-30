d9b0f6e0ca4ef771f81b6bc7673a5b88b1fb49f8
Daniele Petracca <daniele.petracca.dp@gmail.com>
2026-01-30 23:12:01 +0100

    feat: added sales cta section (WIP)

--- app/components/sections/Sale.tsx
+++ app/components/sections/Sale.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
-function Sale() {
+const Sale = () => {
   return (
-    <div>
-      Sale
+    <div className='flex flex-col container items-center gap-8 pt-32 mx-auto relative'>
+      <h2 className='text-4xl md:text-5xl font-bold text-center'> 
+        Limited collection <br/> for sale
+      </h2>
+      <p className='upperxase text-sm font-bold bg-gradient bg-clip-text text-transparent'>discounts up to 30%</p>
+      <Link href='#catalog' className='w-36 flex flex-col items-center py-3 rounded-xl text-xs bg-gradient'>Buy keyboard</Link>
     </div>
   )
 }
