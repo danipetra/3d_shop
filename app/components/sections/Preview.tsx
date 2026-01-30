b40f08b645e86556510aa058ee160fb7f61b7845
Daniele Petracca <daniele.petracca.dp@gmail.com>
2026-01-30 23:21:11 +0100

    feat: added three.js preview section (placeholder geometries, WIP)

--- app/components/sections/Preview.tsx
+++ app/components/sections/Preview.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
+'use client'
 
-function Preview() {
+import React, { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+import {GLTFLoader} from 'three/examples/jsm/Addons.js';
+
+const Preview = () => {
+  const mountRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const mount = mountRef.current;
+    if(!mount) return;
+
+    //TODO const loader = new GLTFLoader();
+
+    const scene = new THREE.Scene();
+    const sceneWidth = window.innerWidth;
+    const sceneHeight = window.innerHeight;
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(sceneWidth, sceneHeight);
+
+    mount.appendChild(renderer.domElement);
+
+    const camera = new THREE.PerspectiveCamera(75, sceneWidth / sceneHeight, 0.1);
+    
+     const geometry = new THREE.BoxGeometry(1,1,1);
+     const material = new THREE.MeshBasicMaterial({color: 0xff00});
+
+    for(let i=0; i<10; i++){
+
+       const cube = new THREE.Mesh(geometry, material);
+       cube.position.set(
+         (Math.random() - 0.5) * 10,
+         (Math.random() - 0.5) * 10,
+         (Math.random() - 0.5) * 10
+       );
+       scene.add(cube);
+     }
+
+    camera.position.z = 6;
+
+    renderer.setAnimationLoop(animate);
+
+    function animate(){
+      scene.traverse((object) => {
+        if(object instanceof THREE.Mesh){  
+          object.rotation.x += 0.01;
+          object.rotation.y += 0.01;
+        }
+      });
+      
+      renderer.render(scene,camera);
+    }
+  })
+
   return (
-    <div>
-      Preview
-    </div>
+    <div ref={mountRef} className='w-screen h-screen'/> //Attaching the scene to this div
   )
 }
 
