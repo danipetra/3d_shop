9820af53e88ec3f41725f5f6acd34a0e0383273a
Daniele Petracca <daniele.petracca.dp@gmail.com>
2026-01-30 23:46:51 +0100

    feat: added catalog section with sold keyboards

--- app/components/sections/Products.tsx
+++ app/components/sections/Products.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Catalog from './Catalog'
+"use client"
+
+import React, { useState } from 'react'
+import Catalog, { ProductType } from './Catalog'
 import Preview from './Preview'
 
-function Products() {
+const Products = () => {
+  const [selectedProduct, setSelectedProduct] = useState<ProductType>({
+    imgSrc: "/assets/keyboard1.png",
+    title: "Magic Keyboard",
+    price: 79.99,
+    modelSrc: "/assets/keyboard.glb", 
+  })
+
+  const handleProductClick = (product: ProductType) =>{
+    setSelectedProduct(product);
+  }
   return (
-    <div>
-      <Catalog/>
-      <Preview/>
+    <div id="catalog" className='max-w-[1536px] flex flex-col mx-auto pt-8'>
+      <Catalog selectedProduct={selectedProduct} onProductClick={handleProductClick}/>
+
+      <Preview selectedProduct={selectedProduct}/>
     </div>
   )
 }
