6e44319667c483ee3f42540aa4c7bfb9857e5654
Daniele Petracca <daniele.petracca.dp@gmail.com>
2026-01-30 23:23:53 +0100

    added loader to Preview section and keyboard asset

--- app/components/sections/Preview.tsx
+++ app/components/sections/Preview.tsx
@@ -10,51 +10,76 @@ const Preview = () => {
     const mount = mountRef.current;
     if(!mount) return;
 
-    //TODO const loader = new GLTFLoader();
+    const loader = new GLTFLoader();
 
     const scene = new THREE.Scene();
-    const sceneWidth = window.innerWidth;
-    const sceneHeight = window.innerHeight;
+    const containerWidth = mount.clientWidth;
+    const sceneWidth = containerWidth < 1536 ? containerWidth : 1536;
+    const sceneHeight = window.innerWidth <= window.innerHeight ? window.innerWidth : window.innerHeight;
 
-    const renderer = new THREE.WebGLRenderer();
+    scene.rotation.x = 3;
+
+    const renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(sceneWidth, sceneHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor(0x000000, 0);
 
     mount.appendChild(renderer.domElement);
 
     const camera = new THREE.PerspectiveCamera(75, sceneWidth / sceneHeight, 0.1);
-    
-     const geometry = new THREE.BoxGeometry(1,1,1);
-     const material = new THREE.MeshBasicMaterial({color: 0xff00});
 
-    for(let i=0; i<10; i++){
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+    scene.add(ambientLight);
 
-       const cube = new THREE.Mesh(geometry, material);
-       cube.position.set(
-         (Math.random() - 0.5) * 10,
-         (Math.random() - 0.5) * 10,
-         (Math.random() - 0.5) * 10
-       );
-       scene.add(cube);
-     }
+    const directionalLightTop = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLightTop.position.set(5, 10, 7.5);
+    scene.add(directionalLightTop);
+    const directionalLightLeft = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLightLeft.position.set(-10, 5, 0);
+    scene.add(directionalLightLeft);
+    const directionalLightRight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLightRight.position.set(10, 5, 0);
+    scene.add(directionalLightRight);
 
-    camera.position.z = 6;
+    loader.load('/assets/keyboard.glb', (gltf) =>{
+      const model = gltf.scene;
+      model.scale.set(1,1,1);
+      model.position.set(0,0,-2);
+      scene.add(model);
+    })
+
+    camera.position.z = 5;
+
+    // const geometry = new THREE.BoxGeometry(1,1,1);
+    // const material = new THREE.MeshBasicMaterial({color: 0xff00});
+
+    // for(let i=0; i<10; i++){
+
+    //   const cube = new THREE.Mesh({geometry, material});
+    //   cube.position.set(
+    //     (Math.random() - 0.5) * 10,
+    //     (Math.random() - 0.5) * 10,
+    //     (Math.random() - 0.5) * 10
+    //   );
+    //   scene.add(cube);
+    // }
 
     renderer.setAnimationLoop(animate);
 
     function animate(){
-      scene.traverse((object) => {
-        if(object instanceof THREE.Mesh){  
-          object.rotation.x += 0.01;
-          object.rotation.y += 0.01;
-        }
-      });
+      // scene.traverse((object) => {
+      //   if(object instanceof THREE.Mesh){  
+      //     object.rotation.x += 0.01;
+      //     object.rotation.y += 0.01;
+      //   }
+      // });
       
       renderer.render(scene,camera);
     }
   })
 
   return (
-    <div ref={mountRef} className='w-screen h-screen'/> //Attaching the scene to this div
+    <div ref={mountRef} className='w-full h-[400px] md:h-[800px] pt-8 md:pt-0'/> //Attaching the scene to this div
   )
 }
 
